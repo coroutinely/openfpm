8ffcf59a2d1e476afbce1d68d50a24a8433a87d2
user <email>
2024-07-12 14:10:45 -0400

    init

--- example/Vector/3_molecular_dynamic_gpu/main.cu
+++ example/Vector/3_molecular_dynamic_gpu/main.cu
@@ -78,7 +78,7 @@
 #include "timer.hpp"
 
 #ifdef TEST_RUN
-size_t nstep = 100;
+size_t nstep = 1;
 #else
 size_t nstep = 1000;
 #endif
@@ -92,6 +92,16 @@ constexpr int force = 1;
 constexpr int energy = 2;
 
 //! \cond [calculate_force_kernel] \endcond
+template<typename vector_dist_type,typename NN_type>
+__global__ void pre_calc_force_gpu(vector_dist_type vd, NN_type NN, real_number sigma12, real_number sigma6, real_number r_cut2)
+{
+	auto p = GET_PARTICLE(vd);
+
+	// Reset the force counter
+	vd.template getProp<force>(p)[0] = 0.0;
+	vd.template getProp<force>(p)[1] = 0.0;
+	vd.template getProp<force>(p)[2] = 0.0;
+}
 
 template<typename vector_dist_type,typename NN_type>
 __global__ void calc_force_gpu(vector_dist_type vd, NN_type NN, real_number sigma12, real_number sigma6, real_number r_cut2)
@@ -101,16 +111,9 @@ __global__ void calc_force_gpu(vector_dist_type vd, NN_type NN, real_number sigm
 	// Get the position xp of the particle
 	Point<3,real_number> xp = vd.getPos(p);
 
-	// Reset the force counter
-	vd.template getProp<force>(p)[0] = 0.0;
-	vd.template getProp<force>(p)[1] = 0.0;
-	vd.template getProp<force>(p)[2] = 0.0;
-
-
-
 	// Get an iterator over the neighborhood particles of p
+	//auto Np = NN.getNNIteratorBoxSym(p, NN.getCell(vd.getPos(p)));
 	auto Np = NN.getNNIteratorBox(NN.getCell(vd.getPos(p)));
-
 	// For each neighborhood particle ...
 	while (Np.isNext())
 	{
@@ -136,9 +139,17 @@ __global__ void calc_force_gpu(vector_dist_type vd, NN_type NN, real_number sigm
 		Point<3,real_number> f = 24.0*(2.0 *sigma12 / (rn*rn*rn*rn*rn*rn*rn) -  sigma6 / (rn*rn*rn*rn)) * r;
 
 		// we sum the force produced by q on p
-		vd.template getProp<force>(p)[0] += f.get(0);
-		vd.template getProp<force>(p)[1] += f.get(1);
-		vd.template getProp<force>(p)[2] += f.get(2);
+		atomicAdd(&vd.template getProp<force>(p)[0], 1);
+		//atomicAdd(&vd.template getProp<force>(p)[1], 1);
+		//atomicAdd(&vd.template getProp<force>(p)[2], 1);
+
+		//atomicAdd(&vd.template getProp<force>(q)[0], 1);
+		//atomicAdd(&vd.template getProp<force>(q)[1], 1);
+		//atomicAdd(&vd.template getProp<force>(q)[2], 1);
+
+		//atomicAdd(&vd.template getProp<force>(q)[0], -f.get(0));
+		//atomicAdd(&vd.template getProp<force>(q)[1], -f.get(1));
+		//atomicAdd(&vd.template getProp<force>(q)[2], -f.get(2));
 
 		// Next neighborhood
 		++Np;
@@ -229,11 +240,62 @@ __global__ void particle_energy(vector_dist_type vd, NN_type NN, real_number sig
 template<typename CellList> void calc_forces(vector_dist_gpu<3,real_number, aggregate<real_number[3],real_number[3],real_number> > & vd, CellList & NN, real_number sigma12, real_number sigma6, real_number r_cut2)
 {
 	vd.updateCellListGPU(NN);
+    NN.debug_deviceToHost();
+    std::cout << "Number of cells :" << NN.getNCells() << std::endl;
+    std::cout << "Elements distribution across NN cells:" << std::endl;
+	int NelementsSum = 0;
+    for (int i = 0; i < NN.getNCells(); i++) {
+        if (NN.getNelements(i) > 0) {
+            std::cout << "[" << i << ": " << NN.getNelements(i) << "] ";
+			NelementsSum += NN.getNelements(i);
+        }
+    }
+	std::cout << std::endl;
+	std::cout << "Total elemenets: " << NelementsSum << std::endl;
+#if 0
+	std::cout << "numPartInCell :" << NN.numPartInCell.size() << std::endl;
+	for (int i = 0; i < NN.numPartInCell.size(); i++) {
+		std::cout << "[" << i << ": " << NN.numPartInCell.template get<0>(i) << "] ";
+	}
+	std::cout << std::endl;
 
+	std::cout << "numPartInCellPrefix :" << NN.numPartInCellPrefixSum.size() << std::endl;
+	for (int i = 0; i < NN.numPartInCellPrefixSum.size(); i++) {
+		std::cout << "[" << i << ": " << NN.numPartInCellPrefixSum.template get<0>(i) << "] ";
+	}
+	std::cout << std::endl;
+#endif
+	auto unsortToSort = NN.getNonSortToSort();
+	std::cout << "unsortedToSortedIndex :" << unsortToSort.size() << std::endl;
+	for (int i = 0; i < unsortToSort.size(); i++) {
+		std::cout << "[" << i << ": " << unsortToSort.template get<0>(i) << "] ";
+	}
+	std::cout << std::endl;
+
+	auto sortToUnsort = NN.getSortToNonSort();
+	std::cout << "sortedToUnortedIndex :" << sortToUnsort.size() << std::endl;
+	for (int i = 0; i < sortToUnsort.size(); i++) {
+		std::cout << "[" << i << ": " << sortToUnsort.template get<0>(i) << "] ";
+	}
+	std::cout << std::endl;
+	
 	// Get an iterator over particles
 	auto it2 = vd.getDomainIteratorGPU();
 
+	CUDA_LAUNCH(pre_calc_force_gpu,it2,vd.toKernel(),NN.toKernel(),sigma12,sigma6,r_cut2);
 	CUDA_LAUNCH(calc_force_gpu,it2,vd.toKernel(),NN.toKernel(),sigma12,sigma6,r_cut2);
+
+	vd.template deviceToHostProp<velocity, force, energy>();
+    auto vd_it = vd.getDomainIterator();
+    while (vd_it.isNext()) {
+        auto key = vd_it.get();
+        std::cout << "(" << vd.getPos(key)[0] << " " << vd.getPos(key)[1] << " " << vd.getProp<0>(key)[2] << ") ";
+		std::cout << "(" << vd.getProp<velocity>(key)[0] << " " << vd.getProp<velocity>(key)[1] << " " << vd.getProp<velocity>(key)[2] << ") ";
+		std::cout << "(" << vd.getProp<force>(key)[0] << " " << vd.getProp<force>(key)[1] << " " << vd.getProp<force>(key)[2] << ") ";
+		std::cout << "{" << vd.getProp<energy>(key) << "} ";
+		std::cout << std::endl;
+        ++vd_it;
+    }
 }
 
 //! \cond [calc_forces] \endcond
@@ -265,10 +327,10 @@ int main(int argc, char* argv[])
 	real_number r_cut = 3.0*sigma;
 
 	// we will use it do place particles on a 10x10x10 Grid like
-	size_t sz[3] = {100,100,100};
+	size_t sz[3] = {100/50,100/50,100/50};
 
 	// domain
-	Box<3,float> box({0.0,0.0,0.0},{1.0,1.0,1.0});
+	Box<3,float> box({0.0,0.0,0.0},{1.0/50,1.0/50,1.0/50});
 
 	// Boundary conditions
 	size_t bc[3]={PERIODIC,PERIODIC,PERIODIC};
@@ -288,6 +350,7 @@ int main(int argc, char* argv[])
 	// We create the grid iterator
 	auto it = vd.getGridIterator(sz);
 
+	int num_particles = 0;
 	while (it.isNext())
 	{
 		// Create a new particle
@@ -312,7 +375,9 @@ int main(int argc, char* argv[])
 		vd.template getLastProp<force>()[2] = 0.0;
 
 		++it;
+		num_particles++;
 	}
+	std::cout << "Num of particles: " << num_particles << std::endl;
 
 	vd.hostToDevicePos();
 	vd.hostToDeviceProp<velocity,force>();
